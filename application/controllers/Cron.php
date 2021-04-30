6dcf65c4a7fd21d81e9c2373d2615798629dfb96
Swakhil <mattapalliswakhil@gmail.com>
2021-04-30 10:42:32 +0530

    updated cron

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -36,6 +36,7 @@ class Cron extends Admin_Controller
 			mkdir('./uploads/invoice/' . $year.'/'.$month, 0777, TRUE);
 		}
   		$store_data = $this->model_stores->getStoresData();
+  		$company_data = $this->model_company->getCompanyData(1);
   		//echo json_encode($store_data);
   		foreach ($store_data as $key => $value) {
   			echo "Printing ".$value['name']." data.";
@@ -47,12 +48,98 @@ class Cron extends Admin_Controller
   				$year = date('Y');
   				$orders_data = $this->model_orders->getUserDeliveriesData($value['id'],$v['id'],$month,$year);
   				echo "Printing ".$v['firstname']." data.";
-  				echo json_encode($orders_data);
+  				//echo json_encode($orders_data);
   				$amount = 0;
   				foreach ($orders_data as $a => $b) {
   					$amount += $b['amount'];
   				}
-  				echo "Total Amount is = ".$amount;
+  				//echo "Total Amount is = ".$amount;
+  				$html = '<!-- Main content -->
+  				<!DOCTYPE html>
+  				<html>
+				<head>
+					<meta charset="utf-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge">
+					<title>Payments</title>
+					<!-- Tell the browser to be responsive to screen width -->
+					<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+					<!-- Bootstrap 3.3.7 -->  
+					<link rel="stylesheet" href="'.base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css').'">
+					<!-- Font Awesome -->  
+					<link rel="stylesheet" href="'.base_url('assets/bower_components/font-awesome/css/font-awesome.min.css').'">
+					<!-- Ionicons -->
+					<link rel="stylesheet" href="'.base_url('assets/bower_components/Ionicons/css/ionicons.min.css').'">
+					<!-- Theme style -->
+					<link rel="stylesheet" href="'.base_url('assets/dist/css/AdminLTE.min.css').'">
+				</head>
+				<body>
+						
+				  <div class="col-md-12 col-xs-12">
+		        	<br>
+
+		        	<div class="box">
+		          		<div class="box-header">
+		            		<h3 class="box-title">Products Delivered for the Selected Month</h3>
+		          		</div>
+		          		<!-- /.box-header -->
+				        <div class="box-body">
+				          <table id="datatables" class="table table-bordered table-striped">
+				            <thead>
+				                <tr style="text-align: center">
+				                  <th style="text-align: center">Date</th>
+				                  <th style="text-align: center">Product Name</th>
+				                  <th style="text-align: center">Quantity</th>
+				                  <th style="text-align: center">Amount</th>
+				                </tr>
+							</thead>
+							<tbody id="tabledata" style="text-align: center">';
+							$exis_date = '';
+							foreach ($orders_data as $k => $v) {
+								$html .= '<tr>';
+								
+								if($exis_date == $v['date']){
+									$html .= '
+						  				<td></td>
+						  				<td>'.$v['product_name'].'</td>
+						  				<td>'.$v['qty'].'</td>
+						  				<td>₹'.$v['amount'].'</td>';
+						  			$exis_date = $v['date'];
+								}else{
+						  			$html .= '
+
+						  				<td>'.$v['date'].'</td>
+						  				<td>'.$v['product_name'].'</td>
+						  				<td>'.$v['qty'].'</td>
+						  				<td>₹'.$v['amount'].'</td>';
+						  			$exis_date = $v['date'];
+						  		}
+						  		$html .= '</tr>';
+						  	}
+						  	$html.='<tr>
+						  				<th colspan="3" style="text-align: center">Gross Amount</th>
+						  				<th style="text-align: center">₹'.$amount.'.00</th>
+						  			</tr>
+						  			<tr>
+						  				<th colspan="3" style="text-align: center">Service Charge</th>
+						  				<th style="text-align: center">₹'.$company_data['service_charge_value'].'</th>
+						  			</tr>';
+						  			$total = $amount + $company_data['service_charge_value'];
+						  	$html.='<tr>
+						  				<th colspan="3" style="text-align: center">Total Amount</th>
+						  				<th style="text-align: center">₹'.$total.'.00</th>
+						  			</tr>
+						  		</tbody>
+		            
+						  </table>
+		          		</div>
+		          		<!-- /.box-body -->
+		        	</div>
+		        	<!-- /.box -->
+
+		          </div>
+				</body>
+				</html>';
+				echo $html;
   				echo "<br>";
   			}
   		}
